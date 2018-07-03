044ed164abba78721302f066090fe90daf8f1c51
DK\IEKA <ieka@sitecore.net>
2018-07-03 19:54:46 +0300

    Fix issue with carousel that has more than 9 slides.

---
+++ src/Sitecore.Support.12975/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.12975")]
+[assembly: AssemblyProduct("Sitecore.Support.12975")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.12975/XA/Feature/Composites/EventHandlers/SaveCompositeItems.cs
@@ -0,0 +1,41 @@
+﻿using Sitecore.XA.Foundation.Presentation.Layout;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Sitecore.Support.XA.Feature.Composites.EventHandlers
+{
+  public class SaveCompositeItems : Sitecore.XA.Feature.Composites.EventHandlers.SaveCompositeItems
+  {
+    protected override void TransformPlaceholderPaths(RenderingModel compositeRendering, int index, IEnumerable<RenderingModel> renderings)
+    {
+      ++index;
+      string dynamicPlaceholderId = this.GetDynamicPlaceholderId(compositeRendering);
+      string pattern = string.Format("(?<={0}+)-{1}-{2}", (object)"section-[title|content]", (object)index, (object)dynamicPlaceholderId);
+      foreach (RenderingModel rendering in renderings)
+      {
+        rendering.Placeholder = GetCompositeSectionPlaceholder(new Placeholder(rendering.Placeholder));
+        rendering.Placeholder = Regex.Replace(rendering.Placeholder, pattern, string.Empty);
+      }
+    }
+
+    public static string GetCompositeSectionPlaceholder(Placeholder ph)
+    {
+      Placeholder placeholder = new Placeholder(ph);
+      string compositeSectionPlaceholderName = GetCompositeSectionPlaceholderName(placeholder);
+      if (compositeSectionPlaceholderName == null)
+      {
+        return null;
+      }
+      int num = placeholder.Parts.LastIndexOf(compositeSectionPlaceholderName);
+      for (int i = 0; i < num; i++)
+      {
+        placeholder.Parts.RemoveAt(0);
+      }
+      return placeholder.GetPlaceholderPath();
+    }
+
+    public static string GetCompositeSectionPlaceholderName(Placeholder ph) =>
+           ph.Parts.LastOrDefault(p => Regex.Match(p, $"{"section-[title|content]"}+-\\d+-\\d").Success);
+  }
+}
